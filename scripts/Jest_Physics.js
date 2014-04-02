6b1deaf2d1b6d628936f08953600e92bd7a79244
U-olympic\Jester <Jester@olympic.(none)>
2014-04-02 00:47:35 -0700

    Fixed jumping into ceilings

--- scripts/Jest_Physics.js
+++ scripts/Jest_Physics.js
@@ -7,39 +7,38 @@ function Gravity(player){
 		SetPersonValue(player, "deltay", parseInt(data["deltay"])+1);
 		
 	
-	if(IsPersonObstructed(player, GetPersonX(player), GetPersonY(player)+1))
+	if(IsPersonObstructed(player, GetPersonX(player)-1, GetPersonY(player)+1)&&IsPersonObstructed(player, GetPersonX(player)+1, GetPersonY(player)+1))
 		SetPersonValue(player, "deltay", 0);
 	
-//	Abort(GetPersonData(player)["deltay"]);
 	for(var i = 0; i<data["deltay"];i+=10){
 		if(IsPersonObstructed(player, GetPersonX(player), GetPersonY(player)+1))
 			return;
 		SetPersonY(player, GetPersonY(player)+1);
 	}
-//		QueuePersonCommand(player, COMMAND_MOVE_SOUTH, true);
 		
 }
 
 function Jump(player){
 
 	if(IsKeyPressed(KEY_SPACE)){
-		if((IsPersonObstructed(player, GetPersonX(player)-2, GetPersonY(player)+1)||IsPersonObstructed(player, GetPersonX(player)+2, GetPersonY(player)+1))
+		if((IsPersonObstructed(player, GetPersonX(player), GetPersonY(player)+1))
 			&&
-		(!((IsPersonObstructed(player, GetPersonX(player)-2, GetPersonY(player)-1)||IsPersonObstructed(player, GetPersonX(player)+2, GetPersonY(player)-1)))))
+		(!(IsPersonObstructed(player, GetPersonX(player), GetPersonY(player)-1)))
+		)
 			SetPersonValue(player, "deltay", -35);
 	}
 
-	if(GetPersonData(player)["deltay"]>=0)
+		
+	if(GetPersonData(player)["deltay"]>0)
 		return;
-		
-	//ClearPersonCommands(player);
-		
+			
 	var data = GetPersonData(player);
 	
-//	Abort(GetPersonData(player)["deltay"]);
 	for(var i = 0; i>data["deltay"];i-=10){
-		if(IsPersonObstructed(player, GetPersonX(player)-2, GetPersonY(player)-1)&&IsPersonObstructed(player, GetPersonX(player)+2, GetPersonY(player)-1))
+		if(IsPersonObstructed(player, GetPersonX(player), GetPersonY(player)-1)){
+			SetPersonValue(player, "deltay", 0);
 			return;
+		}
 		SetPersonY(player, GetPersonY(player)-1);
 	}
 	
