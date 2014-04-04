4fd57c3a4fefae030e3e66c117540fb01cc6d19c
FlyingJester <Jester@olympic.(none)>
2014-04-04 03:20:15 -0700

    Added test font, rolling map switches, and multi-tiling.

--- scripts/Jest_Main.js
+++ scripts/Jest_Main.js
@@ -2,6 +2,10 @@ RequireScript("Jest_Map.js");
 
 RequireScript("Jest_Physics.js");
 
+var ScoreFont = LoadFont("numbers.rfn");
+
+var ph = 100;
+
 function game(){
 	CreatePerson("Egg", "egg.rss", false);
 	AttachInput("Egg");
@@ -11,7 +15,34 @@ function game(){
 	SetPersonValue("Egg", "framei", 0);
 	SetPersonValue("Egg", "frmaxi", 8);
 	SetPersonSpeedXY("Egg", 0, 0);
-	SetUpdateScript(" Jump(\"Egg\"); Gravity(\"Egg\"); Move(\"Egg\");");
+
+		
+	SetUpdateScript("var inp = GetInputPerson(); Jump(inp); Gravity(inp); Move(inp);\
+	var inpx = GetPersonX(inp);\
+	var inpy = GetPersonY(inp);\
+	if(inpx<0){\
+		FJ.Map.West();\
+	}\
+	else if(inpy<0){\
+		FJ.Map.North();\
+	}\
+	else if(inpx>GetLayerWidth(0)*GetTileWidth()){\
+		FJ.Map.East();\
+	}\
+	else if(inpy>GetLayerHeight(0)*GetTileHeight()){\
+		FJ.Map.South();\
+	}\
+	");
+
+	SetRenderScript(
+		"Rectangle(20, 20, 80, 20, CreateColor(0, 0, 0, 255));\
+		Rectangle(110, 20, 450, 20, CreateColor(0, 0, 0, 255));\
+		ScoreFont.drawText(25, 25, ph);\
+		ScoreFont.drawText(115, 25, 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG 1234567890 FLYING JESTER');"
+	);
+
+	BindKey(KEY_COMMA, "ph--", "");
+	BindKey(KEY_PERIOD, "ph++", "");
 
 	MapEngine("test.rmp", 60);
 }
--- scripts/Jest_World.js
+++ scripts/Jest_World.js
@@ -1,3 +1,9 @@
+    Function.prototype.bind = function (self) {
+        var args = Array.prototype.slice.call(arguments, 1), func = this;
+        return function () {
+            return func.apply(self, args.concat(Array.prototype.slice.call(arguments)));
+        };
+    };
 RequireScript("link.js");
 
 const D_L = 1;
@@ -5,34 +11,61 @@ const D_R = 2;
 const D_D = 3;
 const D_U = 4;
 
-function Map(name, x, y){
+if (typeof(FJ)==='undefined')
+	var FJ = {};
+
+function FMap(name, file, x, y){
 	this.name = name;
+	this.file = file;
 	this.x = x;
 	this.y = y;
 }
 
-var CurrentMap = null;
+FJ.Map = {
 
-function SwitchMap(To, From){
-	var direction = D_L;
-	
-	if(To.x==From.x){
-		if(To.y==From.y)
-			return;
+	SwitchMap : function(From, To){
+		
+		if(To.x==From.x){
+			if(To.y==From.y)
+			Abort("Bad Rolling Map Switch: From "+From+" ("+From.x+", "+From.y+") to "+To+" ("+To.x+", "+To.y+").");
 			
+		}
+		else if(To.y==From.y){
+			
+		}
+		else{
+			Abort("Bad Rolling Map Switch: From "+From+" ("+From.x+", "+From.y+") to "+To+" ("+To.x+", "+To.y+").");
+		}
 		
-	}
-	else if(To.y==From.y){
-		
-	}
-	else{
-		Abort("Bad Rolling Map Switch: From "+From.name+" ("+From.x+", "+From.y+") to "+To.name+" ("+To.x+", "+To.y+").");
-	}
+		var inp = GetInputPerson();
+			
+		var inpx= GetPersonX(inp);
+		var inpy= GetPersonY(inp);
+		SetPersonXYFloat(inp, 32, 32);
+		ChangeMap(((To.x<10)?"0":"")+To.x+"-"+((To.y<10)?"0":"")+To.y+".rmp");
+//		ChangeMap(map.file);
+		SetPersonXYFloat(inp, inpx, inpy);
+		this.CurrentMap = To;
+	},
 
+	
+	East : function(){
+		this.SwitchMap(this.CurrentMap, {x:this.CurrentMap.x+1, y:this.CurrentMap.y});
+		SetPersonX(GetInputPerson(), 0);
+	},
+	West : function(){
+		this.SwitchMap(this.CurrentMap, {x:this.CurrentMap.x-1, y:this.CurrentMap.y});
+		SetPersonX(GetInputPerson(), GetLayerWidth(0)*GetTileWidth());
+	},
+	North : function(){
+		this.SwitchMap(this.CurrentMap, {x:this.CurrentMap.x, y:this.CurrentMap.y-1});
+		SetPersonY(GetInputPerson(), GetLayerHeight(0)*GetTileHeight());
+	},
+	South : function(){
+		this.SwitchMap(this.CurrentMap, {x:this.CurrentMap.x, y:this.CurrentMap.y+1});
+		SetPersonY(GetInputPerson(), 0);
+	},
+	
 }
 
-var World = Link([
-	new Map("Test", 0, 0),
-
-
-]);
+FJ.Map.CurrentMap = {x:10, y:10};
